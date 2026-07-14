4aed6352cf17ce304e113918c72b4adb7677f98e
Aebestach <aebestach@163.com>
2026-07-14 13:15:26 +0800

    Migrate build system to KSPBuildTools.

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+// KSP dependency metadata (SDK generates the rest of the assembly attributes).
+[assembly: KSPAssemblyDependency("ClickThroughBlocker", 1, 0)]
+[assembly: KSPAssemblyDependency("ToolbarController", 1, 0)]
